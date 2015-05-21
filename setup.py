92bd06c5be7ca953725152775b7094d15e25d813
Molik, David <dmolik@cshl.edu>
2015-05-21 11:47:39 -0400

    unapplied version changes in setup.py bin/TEtranscripts PKG-INFO README.rst

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
 	       return f.read()
 
 setup(name='TEToolkit',
-      version='1.4.10',
+      version='X.X.X',
       description='Tools for estimating differential enrichment of Transposable Elements and other highly repetitive regions',
       long_description=readme(),
       classifiers=[
