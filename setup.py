274d4cec62c3757f76f7df8f9cd9bc833e872238
Oliver Tam <tam@cshl.edu>
2022-09-02 12:03:29 -0400

    Git Runway Automation for "TEtranscripts", version: "2.2.3" on 2022-09-02

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
                return f.read()
 
 setup(name='TEtranscripts',
-      version='2.2.2',
+      version='2.2.3',
       description='Tools for estimating differential enrichment of Transposable Elements and other highly repetitive regions',
       long_description=readme(),
       classifiers=[
